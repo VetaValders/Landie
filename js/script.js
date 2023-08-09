00110527870ed9707c5def64892600bc62887e21
VetaValders <78078812+VetaValders@users.noreply.github.com>
2023-08-09 14:53:03 +0300

    Добавила прокрутку по клику

--- js/script.js
+++ js/script.js
@@ -20,3 +20,23 @@ function documentClick(e) {
 		document.body.classList.toggle('_lock')
 	}
 }
+//Прокрутка по клику
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
